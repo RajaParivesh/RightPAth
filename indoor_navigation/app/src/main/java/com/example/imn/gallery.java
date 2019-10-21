fbb652c1bebeebe7e750c4d4dc7cd76061c7dc60
RajaParivesh <rajaparivesh@gmail.com>
2019-10-21 18:58:13 +0530

    Indoor Navigation App

---
+++ indoor_navigation/app/src/main/java/com/example/imn/gallery.java
@@ -0,0 +1,25 @@
+package com.example.imn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        Button b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(gallery.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ indoor_navigation/app/src/main/java/com/example/imn/splashscreen.java
@@ -0,0 +1,49 @@
+package com.example.imn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        RelativeLayout l1,l2;
+        ImageView img;
+        Animation uptodown,downtoup;
+        l1=(RelativeLayout) findViewById(R.id.linear);
+        uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        img=(ImageView)findViewById(R.id.imageView);
+        l1.setAnimation(uptodown);
+        img.setAnimation(downtoup);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(splashscreen.this, gallery.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+
+}
